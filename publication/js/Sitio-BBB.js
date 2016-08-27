2f33456e844e81f2db3d7b6095eeb9b2f98f6352
brm-castillol <Lady@MacBook-Pro-de-Lady.local>
2016-08-27 10:08:53 -0500

    second commit

--- publication/js/Sitio-BBB.js
+++ publication/js/Sitio-BBB.js
@@ -1,44 +1,11 @@
-jQuery(document).ready(function($) {
-	jQuery("#form-datosAP").validate();	
-	jQuery(".cont-flecha2,.cont-flecha").click(function(){
-	    var go=jQuery(this).attr("data-go");
-	    var div = jQuery("."+go).position();
-	    jQuery('html, body').animate({scrollTop : div.top},800);
-	    return false;
-	});
-	jQuery(".conten-foto-sub,#BotonSubir").click(function(){
-	  jQuery("#FotoSubir").trigger('click');
-	  return false;
-	});
-	$( "#hielo-1" ).animate({
-   width: "50%",
-  }, 5000, function() {
-    // Animation complete.
-    $("#hielo-2").animate({
-            width: "50%"
-        },5000);
-  });
-  
-});
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  // Elements
-  var scene = document.getElementById('scene');
-  var clipCheckbox = document.getElementById('clip');
-  var relativeCheckbox = document.getElementById('relative');
+    ga('create', 'UA-83174607-1', 'auto');
+    ga('send', 'pageview');
 
-  // Pretty simple huh?
-  var parallax = new Parallax(scene, {
-    relativeInput: relativeCheckbox.checked,
-    clipRelativeInput: clipCheckbox.checked
-  });
-
-    // Elements
-  var scene2 = document.getElementById('scene2');
-  var clipCheckbox2 = document.getElementById('clip2');
-  var relativeCheckbox2 = document.getElementById('relative2');
-
-  // Pretty simple huh?
-  var parallax2 = new Parallax(scene2, {
-    relativeInput: relativeCheckbox2.checked,
-    clipRelativeInput: clipCheckbox2.checked
-  });
+function seguimientoEventos (categoria , accion , label){
+  ga('send', 'event', categoria , accion, label );
+}
