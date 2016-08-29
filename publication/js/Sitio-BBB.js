24bafdba349bccecd0d57361f1b3099b2b21bf6c
brm-castillol <Lady@MacBook-Pro-de-Lady.local>
2016-08-29 17:07:42 -0500

    primera fase

--- publication/js/Sitio-BBB.js
+++ publication/js/Sitio-BBB.js
@@ -9,3 +9,43 @@
 function seguimientoEventos (categoria , accion , label){
   ga('send', 'event', categoria , accion, label );
 }
+/*Slider*/
+
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
+/*MAPA*/
+ var hi = new Vivus('mapa', {type: 'delayed', duration: 30, start: "manual",dashGap: 20, forceRender: true },
+        function () {
+          if (window.console) {
+            //console.log('Animation finished. [log triggered from callback]');N
+            jQuery("#mapa .parte-azul>path").attr('class','relleno-fondo');
+            //jQuery(".parte-morada>path").attr('class','relleno-fondo');
+            //jQuery(".parte-blanca path").addClass('relleno-fondo');
+          }
+        });
+ 
+jQuery(document).ready(function($) {
+  hi.play();
+  jQuery(".regiones-BBB").hover(function() {
+    /* Stuff to do when the mouse enters the element */
+    var Region=jQuery(this).attr('data-region');
+    jQuery("#mapa").find("g path").attr('fill','#00cfff');
+
+    jQuery("#mapa").find("#" + Region + " path").attr('fill','#652d90');
+  }, function() {
+    /* Stuff to do when the mouse leaves the element */
+    jQuery("#mapa").find("g path").attr('fill','#00cfff');
+    
+  });
+});
+
